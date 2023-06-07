7dedf869678066a2c47433aa638d1e60270f5669
naocya@naver.com <naocya@naver.com>
2023-06-07 14:42:05 +0900

    first commit

---
+++ src/main/java/com/example/baseballgamejava/BaseballGameJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.baseballgamejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballGameJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseballGameJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baseballgamejava/BaseballGameJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baseballgamejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballGameJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
